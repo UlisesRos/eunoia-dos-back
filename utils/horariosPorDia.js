eeabb66883d0a5d7efd3e60ed8ad852ec284e6e9
Ulises Ros <ulisesros70@gmail.com>
2025-10-29 17:55:46 -0300

    cambios de horarios

--- utils/horariosPorDia.js
+++ utils/horariosPorDia.js
@@ -1,6 +1,7 @@
 const horariosPorDia = {
     lunes: ['17:00', '18:00', '19:00', '20:00'],
     miércoles: ['17:00', '18:00', '19:00', '20:00'],
+    viernes: ['17:00', '18:00', '19:00'],
 };
 
 module.exports = { horariosPorDia };
